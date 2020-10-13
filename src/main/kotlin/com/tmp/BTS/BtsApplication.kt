c5f11093bf18b5bb99a8e2ae629fbd02c83b4f4b
namziji <namzisun@gmail.com>
2020-10-13 15:52:31 +0900

    project init

---
+++ src/main/kotlin/com/tmp/BTS/BtsApplication.kt
@@ -0,0 +1,11 @@
+package com.tmp.BTS
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BtsApplication
+
+fun main(args: Array<String>) {
+	runApplication<BtsApplication>(*args)
+}
---
+++ src/test/kotlin/com/tmp/BTS/BtsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tmp.BTS
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BtsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
